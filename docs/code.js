4608a3f56e25d4939f5d15c6aa0a2be456035ec2
Rettend <hegyi.aron101@gmail.com>
2022-04-07 20:04:42 +0200

    wow google drive iframe popping off

---
+++ docs/code.js
@@ -0,0 +1,12 @@
+// Window leave confirmation
+// window.onbeforeunload = function (e) {
+//     return "야~ 무슨 생각을 하고 있니? ";
+// };
+
+// const songs = new Audio("songs/*");
+
+
+
+const random = Math.floor(Math.random() * months.length);
+console.log(random, songs[random]);
+
